9ab77976bd119b6e75cf7bb91c2744b136ed19c1
Asadullah-Imran <asadullahimran19@gmail.com>
2024-01-13 16:56:26 +0600

    building game state

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -166,7 +166,7 @@ public class Player extends Entity{
                     break;
                 case "Boots":
                     gp.playSE(2);
-                    speed+=4;
+                    speed+=1;
                     gp.obj[i] =null;
                     System.out.println("Boots Found");
                     break;
